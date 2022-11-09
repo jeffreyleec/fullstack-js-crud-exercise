4affb832ee7c9af351f47455f30e519be4e1d592
Jeffrey Lee <jeffrey-lee@live.ca>
2022-11-09 16:00:28 -0500

    table generated with rows and columns

--- client/plexxis-exercise/src/components/Table.js
+++ client/plexxis-exercise/src/components/Table.js
@@ -1,14 +1,134 @@
-import React from 'react'
-import {useTable }from 'react-table'
-import {mockData} from './mockData'
-import {Columns} from './Columns'
+import React, { useMemo } from "react";
+import { useTable } from "react-table";
+import { mockData } from "./mockData";
+import { Columns } from "./Columns";
 
 export const Table = () => {
+  // console.log(mockData)
+  // console.log(Columns)
 
-  console.log(mockData)
-  console.log(Columns)
+  //only refresh on memoized value changes
+   const columns = useMemo(() => Columns, []);
+   const data = useMemo(() => mockData, []);
 
+  // const tableInstance = useTable({
+  //   columns,
+  //   data,
+  // });
+
+  // const { getTableProps, getTableBodyProps, headerGroups, rows, prepareRow, getCellProps } =
+  //   tableInstance;
+
+  // const data = React.useMemo(
+  //   () => [
+  //     {
+  //       col1: 'Hello',
+  //       col2: 'World',
+  //     },
+  //     {
+  //       col1: 'react-table',
+  //       col2: 'rocks',
+  //     },
+  //     {
+  //       col1: 'whatever',
+  //       col2: 'you want',
+  //     },
+  //   ],
+  //   []
+  // )
+  // const columns = React.useMemo(
+  //   () => [
+  //     {
+  //       Header: 'Column 1',
+  //       accessor: 'col1', // accessor is the "key" in the data
+  //     },
+  //     {
+  //       Header: 'Column 2',
+  //       accessor: 'col2',
+  //     },
+  //   ],
+  //   []
+  // )
+  const tableInstance = useTable({ columns, data })
+
+  const {
+    getTableProps,
+    getTableBodyProps,
+    headerGroups,
+    rows,
+    prepareRow,
+  } = tableInstance
+  
   return (
-    <div>Table</div>
+    // apply the table props
+    <table {...getTableProps()}>
+      <thead>
+        {// Loop over the header rows
+        headerGroups.map(headerGroup => (
+          // Apply the header row props
+          <tr {...headerGroup.getHeaderGroupProps()}>
+            {// Loop over the headers in each row
+            headerGroup.headers.map(column => (
+              // Apply the header cell props
+              <th {...column.getHeaderProps()}>
+                {// Render the header
+                column.render('Header')}
+              </th>
+            ))}
+          </tr>
+        ))}
+      </thead>
+      {/* Apply the table body props */}
+      <tbody {...getTableBodyProps()}>
+        {// Loop over the table rows
+        rows.map(row => {
+          // Prepare the row for display
+          prepareRow(row)
+          return (
+            // Apply the row props
+            <tr {...row.getRowProps()}>
+              {// Loop over the rows cells
+              row.cells.map(cell => {
+                // Apply the cell props
+                return (
+                  <td {...cell.getCellProps()}>
+                    {// Render the cell contents
+                    cell.render('Cell')}
+                  </td>
+                )
+              })}
+            </tr>
+          )
+        })}
+      </tbody>
+    </table>
   )
-}
+ 
+    // <table {...getTableProps}>
+    //   <thead>
+    //     {headerGroups.map((headerGroup) => (
+    //       <tr>
+    //         {" "}
+    //         {...headerGroup.getHeaderGroupProps()}
+    //         {headerGroup.headers.map((column) => (
+    //           <th {...column.getHeaderProps()}>{column.render("Header")}</th>
+    //         ))}
+    //       </tr>
+    //     ))}
+    //   </thead>
+
+    //   <tbody {...getTableBodyProps()}>
+    //     {rows.map((row) => {
+    //       prepareRow(row);
+    //       return (
+    //         <tr {...row.getRowProps()}>
+    //           {row.cells.map((cell) => {
+    //             return <td {...getCellProps()}>{cell.render("Cell")}</td>;
+    //           })}
+    //         </tr>
+    //       );
+    //     })}
+    //   </tbody>
+    // </table>
+  
+};
